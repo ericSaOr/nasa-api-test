93194ec58d74c3c1523b586d600b55c0b35cf73f
Eric Sanchez <ericfsod@gmail.com>
2024-01-16 13:55:24 -0500

    Pulled data from NASA APOTD API, working on adding a date picker

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const url="https://api.nasa.gov/planetary/apod?api_key=" 
 const api_key = "dKbq9NCkQoy3F8pbmkjH5D2cBLRaYzbK1EkgO5Hx";
+// const api_key = config.NASA_API_KEY
 console.log(api_key)
 
 const fetchNasaPhotos = async () =>{
@@ -8,14 +9,21 @@ const fetchNasaPhotos = async () =>{
         const response = await fetch(`${url}${api_key}`)
         const data = await response.json()
         console.log('NASA APOD data', data)
-            
+            displayData(data)
         
     }catch(error){
         console.log(error)
     }
 }
 
+
+
+const displayData = data => {
+    document.getElementById('title').textContent = data.title
+    document.getElementById('date').textContent = data.date
+    document.getElementById('picture').src = data.url
+    document.getElementById('blurb').textContent = data.explanation
+}
+
+
 fetchNasaPhotos()
-
-
-
